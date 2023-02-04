2dede309a96eee0366dc3fcd41593749796612c2
Jiho Im <102697064+Joshua0323@users.noreply.github.com>
2023-02-04 16:12:35 -0500

    Time: 10 ms (57.89%), Space: 42.8 MB (40.61%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() > s2.length()) return false;
+
+        Integer[] freq = countFreq(s1);
+        int n = s2.length(), left = 0, right = 0;
+        int count = s1.length();
+        while (right < n) {
+            char c = s2.charAt(right);
+            // Verify and move right
+            if (freq[c] == null) {// reset
+                freq = countFreq(s1);
+                left = ++right;
+                count = s1.length();
+                continue;
+            }
+            right++;
+            count--;
+            freq[c]--;
+            
+            // Move left when necessary
+            while(freq[c] < 0) {
+                freq[s2.charAt(left++)]++;
+                count++;
+            }
+            
+            // verify:
+            if (count == 0) return true;
+        }
+        return count == 0;
+    }
+    
+    private Integer[] countFreq(String s) {
+        Integer[] freq = new Integer[256];
+        for (char c : s.toCharArray()) freq[c] = freq[c] == null ? 1 : freq[c] + 1;
+        return freq;
+    }
+}
