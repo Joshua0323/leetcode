3fffb845e4782b7addd233b164710ac855a74002
Jiho Im <102697064+Joshua0323@users.noreply.github.com>
2023-09-29 23:23:56 -0400

    Time: 3 ms (98.13%), Space: 63.2 MB (44.78%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int min = Integer.MIN_VALUE;
+        int peak = nums.length;
+        for (int i = nums.length - 1; i >= 0; i--) {
+
+            if (nums[i] < min) {
+                return true;               
+            }
+
+
+            while (peak < nums.length && nums[i] > nums[peak]) {
+                min = nums[peak++];               
+            }
+            peak--;
+            nums[peak] = nums[i];
+        }
+        return false;
+    }
+}
