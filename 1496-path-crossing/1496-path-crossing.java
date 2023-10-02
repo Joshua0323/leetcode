633bf65157e0f29accd330a850d6db034b8c03bc
Jiho Im <102697064+Joshua0323@users.noreply.github.com>
2023-10-01 20:30:31 -0400

    Time: 5 ms (46.83%), Space: 40.8 MB (81.19%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        Set<String> set = new HashSet<>();
+        set.add("(0,0)");
+        int x = 0;
+        int y = 0;
+        
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == 'N') {
+                y++;
+            } else if (path.charAt(i) == 'S') {
+                y--;
+            } else if (path.charAt(i) == 'E') {
+                x++;
+            } else {
+                x--;
+            }
+            String s = "(" + x + "," + y + ")";
+            if (set.contains(s)) {
+                return true;
+            } else {
+                set.add(s);
+            }
+        }
+        return false;
+    }
+}
