1b162187d163f8049f32a116c5542059d2d3c09e
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2023-10-03 10:52:02 -0400

    Time: 1 ms (86.48%), Space: 39.8 MB (60.02%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -3,7 +3,7 @@ class Solution {
         // int count=0;
         // for(int i=0;i<nums.length;i++){
         //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i]==nums[j])
+        //         if(nums[i]==nums[j]&&i>j)
         //             count++;
         //     }
         // }
