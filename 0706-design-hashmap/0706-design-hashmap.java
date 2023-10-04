47e46c25bcb1573b12287ecdd33604843f3cabf5
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2023-10-04 10:32:35 -0400

    Time: 30 ms (39.67%), Space: 54.3 MB (23.87%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,80 +1,21 @@
 class MyHashMap {
-    static class Node {
-        int key, value;
-        Node next;
-        Node (int _key, int _value) {
-            key = _key;
-            value = _value;
-        }
-    }
-
-    Node[] nodes = new Node[10006];
-
+    int[] data;
+    
     public MyHashMap() {
-        
+        data = new int[1000001];
+        Arrays.fill(data, -1);
     }
     
-    public void put(int key, int value) {
-        // Collision Handling in put: 
-        // In the put method, 
-        // you are updating the value if the key already exists. 
-        // However, you should also check if the key exists when the cur.next is null 
-        // (i.e., at the end of the linked list).
-        int index = key % nodes.length;
-        if (nodes[index] != null) {
-            Node prev = null;
-            Node cur = nodes[index];
-            while (cur != null && cur.key != key) {
-                prev = cur;
-                cur = cur.next;
-            }
-
-            if (cur != null) {
-                cur.value = value;
-            } else {
-                Node item = new Node(key, value);
-                prev.next = item;
-            }
-        } else {
-            Node item = new Node(key, value);
-            nodes[index] = item;
-        }
+    public void put(int key, int val) {
+        data[key] = val;
     }
     
     public int get(int key) {
-        int index = key % nodes.length;
-        if (nodes[index] != null) {
-            Node cur = nodes[index];
-            while (cur != null) {
-                if (cur.key == key) {
-                    return cur.value;
-                }
-                cur = cur.next;
-            }
-        } 
-        return -1;
+        return data[key];
     }
     
     public void remove(int key) {
-        // Remove Method: The remove method is missing handling for removing the first node in the linked list. 
-        // You should update the nodes[index] reference if you remove the first node.
-        int index = key % nodes.length;
-        if (nodes[index] != null) {
-            Node prev = null;
-            Node cur = nodes[index];
-            while (cur != null && cur.key != key) {
-                prev = cur;
-                cur = cur.next;
-            }
-
-            if (cur != null) {
-                if (prev != null) {
-                    prev.next = cur.next;
-                } else {
-                    nodes[index] = cur.next;
-                }
-            }
-        }          
+        data[key] = -1;
     }
 }
 
