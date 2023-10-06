d786981c4d32683867632ce1af6d2323fe16d58a
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2023-10-06 09:46:02 -0400

    Time: 0 ms (100.00%), Space: 39.2 MB (39.77%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int integerBreak(int n) {
+        
+        int max = 0;
+        
+        if(n == 2) {
+            max = 1;
+        } else if (n == 3) {
+            max = 2;
+        }
+        
+        
+        if(n > 3) {
+            if (n % 3 == 0) {
+                max = (int) Math.pow(2, 0) * (int) Math.pow(3, n/3);
+            }
+            else if (n % 3 == 1) {
+                max = (int) Math.pow(2, 2) * (int) Math.pow(3, n/3 - 1); 
+            } else if (n % 3 == 2){
+                max = (int) Math.pow(2, 1) * (int) Math.pow(3, n/3); 
+            }
+        }
+ 
+        return max;
+        
+    }
+}
