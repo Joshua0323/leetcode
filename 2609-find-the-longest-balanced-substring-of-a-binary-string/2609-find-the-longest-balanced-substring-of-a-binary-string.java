f0a79300d5bdeb7aba44b30c98bc446e7507ab09
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2023-10-10 17:46:02 -0400

    Time: 2 ms (60.71%), Space: 41.4 MB (96.43%) - LeetHub

--- 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.java
+++ 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.java
@@ -1,12 +1,23 @@
 class Solution {
     public int findTheLongestBalancedSubstring(String s) {
-        int res = 0;
-        String temp = "01";
-        while(temp.length() <= s.length()){
-            if(s.contains(temp))
-                res = temp.length();
-            temp = "0" + temp + "1";
+        int cnt0 = 0, cnt1 = 0;
+        int maxLen = 0;
+        for (int i = 0; i < s.length();) {
+            // Set the window
+            // count the number of zeros
+            while (i < s.length() && s.charAt(i) == '0') {
+                cnt0++; i++;
+            }
+            // count the number of ones
+            while (i < s.length() && s.charAt(i) == '1') {
+                cnt1++; i++;
+            }
+            // Do the pairing
+            maxLen = Math.max(maxLen, 2 * Math.min(cnt0, cnt1));
+            // reset the values (for new pairs)
+            cnt0 = cnt1 = 0;    
         }
-        return res;
+        
+        return maxLen;
     }
 }
