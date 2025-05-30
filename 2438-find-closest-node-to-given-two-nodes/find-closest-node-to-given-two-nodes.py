ca7c579e603e78c331577b74907a20ca265a28d7
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-05-29 22:55:45 -0400

    Time: 116 ms (94.12%) | Memory: 26.1 MB (76.47%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.py
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def dfs(self, current, distance, edges, distances):
+        # self.dfs(-1, 3, [1,2,-1], [0, 1, 2])
+        # self.dfs(-1, 1, [1,2,-1], [-1, -1, 0])
         while current != -1 and distances[current] == -1:
             distances[current] = distance
             distance += 1
@@ -9,11 +11,14 @@ class Solution(object):
         res, Min_Of_Max, n = -1, float('inf'), len(edges)
         dist1 = [-1] * n
         dist2 = [-1] * n
+        # print(2<Min_Of_Max)
         self.dfs(node1, 0, edges, dist1)
         self.dfs(node2, 0, edges, dist2)
+        # self.dfs(-1, 3, [1,2,-1], [0, 1, 2])
+        # self.dfs(-1, 1, [1,2,-1], [-1, -1, 0])
         for i in range(n):
             if dist1[i] >= 0 and dist2[i] >= 0:
-                maxDist = max(dist1[i], dist2[i])
+                maxDist = max(dist1[i], dist2[i])  # 2
                 if maxDist < Min_Of_Max:
                     Min_Of_Max = maxDist
                     res = i
