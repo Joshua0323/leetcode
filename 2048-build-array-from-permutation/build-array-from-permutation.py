ece1d5383297a04c9550a91acd32cc25d106de9d
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-06-01 16:00:51 -0400

    Time: 7 ms (21.55%) | Memory: 12.7 MB (30.60%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        res = []
-        for i  in nums:
-            res.append(nums[i])
+        res = [-1]*len(nums)
+        for i  in range(len(nums)):
+            res[i] = nums[nums[i]]
         return res
