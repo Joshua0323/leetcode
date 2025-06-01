dc70786a1fa79664afa04cfe87fd90826bd7a85f
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-06-01 18:14:06 -0400

    Time: 4 ms (89.53%) | Memory: 12.3 MB (98.71%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,30 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        nums = str(abs(x))
-        n = len(str(x))
-
-        first_half = []
-        second_half = []
-        res = True
-
-        if x < 0:
+        x2=str(x)
+        num=x2[::-1]
+        if num==x2:
+            return True
+        else:
             return False
-        elif x < 10:
-            return res
-        elif x == 10:
-            return False
-        elif x < 100 and (x % 10 == x / 10):
-            return res
-
-        for i in range(n/2):
-            first_half.append(nums[i])
-            second_half.append(nums[-i-1])
-            if first_half[i] != second_half[i]:
-                return False
-
-        print(first_half)
-        print(second_half)
-
-        return res
 
