5b732b6ab1545eac04d836493a9de1e18729f505
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-06-01 16:00:19 -0400

    Time: 4 ms (44.12%) | Memory: 12.5 MB (92.39%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for i  in nums:
+            res.append(nums[i])
+        return res
