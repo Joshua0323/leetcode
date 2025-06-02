c5069f365a6a70b58eccce2fd0b55d7003572b68
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-06-02 00:55:34 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (47.68%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """       
-        for i in range(len(nums) - 1):
-            if (nums[i] % 2 == 0 and nums[i + 1] % 2 == 0) or (nums[i] % 2 != 0 and nums[i + 1] % 2 != 0):
+        for i in range(len(nums)-1):
+            if (nums[i]%2==0 and nums[i+1]%2==0) or (nums[i]%2!=0 and nums[i+1]%2!=0):
                 return False
         return True
         
