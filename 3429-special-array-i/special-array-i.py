48ad6e01d78f63a772abd5f73a48db73505b2fc3
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-06-02 00:54:52 -0400

    Time: 3 ms (34.06%) | Memory: 12.4 MB (47.68%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -3,13 +3,9 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-        """
-        res = True
-        if len(nums) == 1:
-            return True
-        else:
-            for i in range(len(nums) - 1):
-                if (nums[i] % 2 == 0 and nums[i + 1] % 2 == 0) or (nums[i] % 2 != 0 and nums[i + 1] % 2 != 0):
-                    return False
-
-        return res
+        """       
+        for i in range(len(nums) - 1):
+            if (nums[i] % 2 == 0 and nums[i + 1] % 2 == 0) or (nums[i] % 2 != 0 and nums[i + 1] % 2 != 0):
+                return False
+        return True
+        
