41f3fae76df8dcf7bb51d30f708ec7991789d322
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-06-12 12:55:11 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.53%) - LeetSync

--- 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.py
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.py
@@ -4,15 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max_num = 0
-        min_num = 0
-        res = 0
-
-        for i in range(len(nums) - 1):
-            res = max(res, abs(nums[i] - nums[i + 1])) 
-
-        res = max(res, abs(nums[len(nums) - 1] - nums[0]))
-
+        res=0
+        for i in range(len(nums)-1):
+            res=max(res,abs(nums[i]-nums[i+1])) 
+        res=max(res,abs(nums[len(nums)-1]-nums[0]))
         return res
 
         
