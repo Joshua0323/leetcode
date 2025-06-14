00aef0583e25f5d781f28d0b27340a1a2ee4a689
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-06-13 22:15:30 -0400

    Time: 467 ms (45.00%) | Memory: 21.7 MB (95.00%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
@@ -7,7 +7,8 @@ class Solution(object):
         """
         nums.sort()
         low = 0
-        high = int(1e9)
+        # high = int(1e9)
+        high = nums[len(nums)-1] - nums[0]
         while low < high:
             i = 1
             count = 0
@@ -17,8 +18,8 @@ class Solution(object):
                     count += 1
                     i += 1
                 i += 1
-            if count >= p:
-                high = diff
-            else:
+            if count < p:
                 low = diff + 1
+            else:
+                high = diff
         return low
