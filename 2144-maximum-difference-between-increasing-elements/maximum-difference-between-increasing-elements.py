29e29a3a2748fa066ca50211d52a6becbf50dbf5
Joshua Im <102697064+Joshua0323@users.noreply.github.com>
2025-06-16 11:05:56 -0400

    Time: 183 ms (21.72%) | Memory: 12.4 MB (98.48%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.py
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max = -1
-        n = len(nums)
-        
-        for i in range(1, n+1):
-            for j in range(i + 1, n+1):
-                is_max = nums[-i] - nums[-j]
-                # print(nums[-i], nums[-j], nums[-i] > nums[-j])
-                if is_max > max and nums[-i] > nums[-j] :
-                    max = is_max
-
+        max,n=-1,len(nums)
+        for i in range(1,n+1):
+            for j in range(i+1,n+1):
+                is_max=nums[-i]-nums[-j]
+                if is_max>max and nums[-i]>nums[-j] :
+                    max=is_max
         return max            
